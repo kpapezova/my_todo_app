b1270511ba0359a44921c5159ad45588732647ad
kpapezova <papezka.kristyna@gmail.com>
2023-05-01 10:43:32 +0200

    Update method was added

--- app.py
+++ app.py
@@ -43,8 +43,26 @@ def delete(id):
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
+        return redirect('/')
     except:
         return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task_to_update)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
