8b553017a556000264425cad2e3d5364094d1b34
kpapezova <papezka.kristyna@gmail.com>
2023-05-02 20:55:51 +0200

    Final check

--- app.py
+++ app.py
@@ -7,13 +7,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-with app.app_context():
-    db.create_all()
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
